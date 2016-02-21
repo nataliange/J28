5a7d5330cca8c12f836f3e3e50f1045f597dfb70
nataliange <natalia.gegenava@gmail.com>
2016-02-21 19:54:14 +0100

    changes for gradle
    changes for gradle

---
+++ sandbox/src/main/java/spb/tests/GradleSandbox.java
@@ -0,0 +1,11 @@
+package spb.tests;
+
+public class GradleSandbox {
+
+	public static void main(String[] args) {
+		String s = "gradle";
+		String s1 = s.substring(1, s.length());
+		System.out.println(s1);
+	}
+
+}
